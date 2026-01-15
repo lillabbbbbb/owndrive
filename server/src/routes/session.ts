67ecaee66efa3c647d32c9a7db3a8fceaecd13e0
lillabbbbbb <128832523+lillabbbbbb@users.noreply.github.com>
2026-01-15 14:51:51 +0200

    implemented session sorting and pagination UPDATE routes

--- server/src/routes/session.ts
+++ server/src/routes/session.ts
@@ -88,6 +88,7 @@ sessionRouter.patch(":user/session/filter/remove",
     }
 })
 
+//UPDATE remove all filters
 //params: username
 sessionRouter.patch(":user/session/filter/remove_all",
     async (req: Request, res: Response) => {
@@ -123,6 +124,69 @@ sessionRouter.patch(":user/session/filter/remove_all",
 
 //UPDATE change sorting
 //params: sorting: string (e.g. "newest first")
+sessionRouter.patch(":user/session/filter/remove_all",
+    async (req: Request, res: Response) => {
+    try {
+
+        //check if username (:user) matches the user signed inside the jwt token
+        
+
+        //get the right user
+        const user = await User
+            .findOne({username: req.body.username})
+            .select("session")
+
+        //if user not found
+        if (!user) return res.status(404).json({message: 'User not found'})
+
+        //otherwise save the new image in the image db
+        //Note: no need to update the parent (user) record since the Image was referenced, not embedded, in the model declaration
+        await Session.findByIdAndUpdate(
+            user._id,
+            {$set: {
+                sorting: req.body.sorting
+            } },
+            {new: true}
+        )
+        
+        return res.status(200).json({"message": `${req.body.sorting} sorting applied successfully.`})
+    } catch (error: any) {
+        console.log(error)
+        return res.status(500).json({"message": "Internal Server Error"})
+    }
+})
 
 //UPDATE change page
 //params: page_num: number
+
+sessionRouter.patch(":user/session/filter/remove_all",
+    async (req: Request, res: Response) => {
+    try {
+
+        //check if username (:user) matches the user signed inside the jwt token
+        
+
+        //get the right user
+        const user = await User
+            .findOne({username: req.body.username})
+            .select("session")
+
+        //if user not found
+        if (!user) return res.status(404).json({message: 'User not found'})
+
+        //otherwise save the new image in the image db
+        //Note: no need to update the parent (user) record since the Image was referenced, not embedded, in the model declaration
+        await Session.findByIdAndUpdate(
+            user._id,
+            {$set: {
+                pagination_current: req.body.page_num
+            } },
+            {new: true}
+        )
+        
+        return res.status(200).json({"message": `Current page value (${req.body.page_num}) saved successfully.`})
+    } catch (error: any) {
+        console.log(error)
+        return res.status(500).json({"message": "Internal Server Error"})
+    }
+})
