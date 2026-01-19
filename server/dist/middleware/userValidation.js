55b81baeec8c57fac5227b4d1f55c96e001acc34
lillabbbbbb <128832523+lillabbbbbb@users.noreply.github.com>
2026-01-19 15:15:01 +0200

    added user and admin validation methods, not used in the backend routes yet

--- server/dist/middleware/userValidation.js
+++ server/dist/middleware/userValidation.js
@@ -1,4 +1,46 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 //user and admin validation (checking valid token, checking access)
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.validateAdminToken = exports.validateUserToken = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const validateUserToken = (req, res, next) => {
+    const token = req.header("authorization")?.split(" ")[1];
+    if (!token)
+        return res.status(401).json({ message: "Token not found." });
+    try {
+        const verified = jsonwebtoken_1.default.verify(token, process.env.SECRET);
+        req.user = verified;
+        next();
+    }
+    catch (error) {
+        console.log(error);
+        res.status(401).json({ message: "Token not found." });
+    }
+};
+exports.validateUserToken = validateUserToken;
+const validateAdminToken = (req, res, next) => {
+    console.log("Authorization header:", req.header("authorization"));
+    const token = req.header("authorization")?.split(" ")[1];
+    if (!token)
+        return res.status(403).json({ message: "Token not found" });
+    console.log("token found");
+    try {
+        const verified = jsonwebtoken_1.default.verify(token, process.env.SECRET);
+        req.user = verified;
+        if (!req.user.isAdmin) {
+            return res.status(403).json({ message: "Access denied." });
+        }
+        next();
+    }
+    catch (error) {
+        console.log(error);
+        res.status(403).json({ message: "Access denied." });
+    }
+};
+exports.validateAdminToken = validateAdminToken;
 //# sourceMappingURL=userValidation.js.map
--- server/src/middleware/userValidation.ts
+++ server/src/middleware/userValidation.ts
@@ -1,1 +1,51 @@
 //user and admin validation (checking valid token, checking access)
+
+import {Request, Response, NextFunction} from "express"
+import jwt, { JwtPayload } from "jsonwebtoken"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export interface CustomRequest extends Request {
+    user?: JwtPayload
+}
+
+export const validateUserToken = (req: CustomRequest, res: Response, next: NextFunction) => {
+    const token: string | undefined = req.header("authorization")?.split(" ")[1]
+
+    if(!token) return res.status(401).json({message: "Token not found."})
+
+
+    try {
+        const verified: JwtPayload = jwt.verify(token, process.env.SECRET as string) as JwtPayload
+        req.user = verified
+        next()
+
+    }  catch (error: any) {
+        console.log(error)
+        res.status(401).json({message: "Token not found."})
+    }
+}
+
+export const validateAdminToken = (req: CustomRequest, res: Response, next: NextFunction) => {
+    console.log("Authorization header:", req.header("authorization"));
+    const token: string | undefined = req.header("authorization")?.split(" ")[1]
+
+    if(!token) return res.status(403).json({message: "Token not found"})
+    console.log("token found")
+
+
+    try {
+        const verified: JwtPayload = jwt.verify(token, process.env.SECRET as string) as JwtPayload
+        req.user = verified
+
+        if (!req.user.isAdmin){
+            return res.status(403).json({message: "Access denied."})
+        }
+        next()
+
+    }  catch (error: any) {
+        console.log(error)
+        res.status(403).json({message: "Access denied."})
+    }
+}
