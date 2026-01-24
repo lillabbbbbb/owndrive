2f7e7ffb0828175d023e0b5cae80fac97bbb0531
lillabbbbbb <128832523+lillabbbbbb@users.noreply.github.com>
2026-01-24 19:32:55 +0200

    added comments in frontend,  to where communication with the server will be

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,14 @@ import { useEffect, useState } from 'react'
 function App() {
 
   const [jwt, setJwt] = useState<string | null>(null)
+  //from server fetch: logged in user's personal data (User)
+  const [userData, setUserData] = useState(null)
+  //from server fetch: logged in user's files (File[])
+  const [userFiles, setUserFiles] = useState(null)
+
+  const [language, setLanguage] = useState(null)
+  const [mode, setMode] = useState(null)
+
 
   useEffect(() => {
         if(localStorage.getItem("token")) {
